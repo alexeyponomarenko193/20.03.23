b68c058e8e5b63d74b2b2cc0efea0e1bc31bb83f
alexeyponomarenko193 <127084895+alexeyponomarenko193@users.noreply.github.com>
2023-03-20 09:36:03 +0200

    Add files via upload

---
+++ ConsoleApp4444/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleApp4444
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine();
+            //int n = 10;
+            //double[] masses = { 2.5, 3.7, 1.2, 5.3, 2.1, 6.0, 4.2, 2.8, 3.5, 1.9 }; 
+            //double totalMass = 0; 
+            //for (int i = 0; i < n; i++)
+            //{
+            //    totalMass += masses[i];
+            //}
+
+            //Console.WriteLine("Общая масса груза: " + totalMass + " кг");
+            //Console.ReadKey();
+            //int n = 30;
+            //double[] heights = { 170.2, 172.5, 169.8, 173.6, 171.1, 168.9, 175.3, 174.2, 170.7, 176.4, 173.8, 172.1, 171.7, 175.1, 169.4, 172.8, 174.6, 176.0, 170.9, 173.5, 171.9, 172.4, 170.5, 173.2, 175.5, 174.9, 169.7, 172.0, 175.7, 171.4, 174.3 }; // массив ростов студентов
+            //double averageHeight = 0;
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    averageHeight += heights[i];
+
+            //    averageHeight /= n;
+
+            //    Console.WriteLine("Средний рост студентов: " + averageHeight + " см");
+            //    Console.ReadKey();
+            //}
+            //int n = 15; 
+            //int[] arr = new int[n]; 
+            //Random rnd = new Random();
+            //int positiveCount = 0; 
+            //int negativeCount = 0; 
+            //int zeroCount = 0;
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 11);
+
+            //    if (arr[i] > 0)
+            //    {
+            //        positiveCount++; 
+            //    }
+            //    else if (arr[i] < 0)
+            //    {
+            //        negativeCount++;
+            //    }
+            //    else
+            //    {
+            //        zeroCount++;
+            //    }
+            //}
+
+            //Console.WriteLine("Положительных элементов: " + positiveCount);
+            //Console.WriteLine("Отрицательных элементов: " + negativeCount);
+            //Console.WriteLine("Нулевых элементов: " + zeroCount);
+            //Console.ReadKey();
+            //    int[] arr = { 2, 3, 4, 6, 8, 7, 10, 12, 9, 11 };
+            //    int n = arr.Length;
+            //    int evenCount = 0;
+
+            //    for (int i = 0; i < n; i++)
+            //    {
+            //        if (arr[i] % 2 == 0)
+            //        {
+            //            evenCount++;
+            //        }
+            //    }
+
+            //    Console.WriteLine("Четных элементов: " + evenCount);
+            //    Console.ReadKey();
+            //}
+        }
+    }
+}
